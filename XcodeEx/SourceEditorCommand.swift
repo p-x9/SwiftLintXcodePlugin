f961d3ca972222c3d3b60da7a726e488ae5bb015
p-x9 <chi.iosdev@gmail.com>
2021-05-22 00:19:31 +0900

    Add SourceEditorExtension Target

---
+++ XcodeEx/SourceEditorCommand.swift
@@ -0,0 +1,20 @@
+//
+//  SourceEditorCommand.swift
+//  SwiftLintPlugin
+//
+//  Created by p-x9 on 2021/05/22.
+//  
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
